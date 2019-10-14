3b0cef7a7a17acbddc298395dc3ff31d41c7f68d
buituan11 <buituan71099@gmail.com>
2019-10-15 00:40:59 +0700

    Updates

---
+++ precache-manifest.f2a74289d20601cb438b1e98fd72843d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b2d6a7515e4134749ee6d15ef5bea6",
+    "url": "/hbpddemo/index.html"
+  },
+  {
+    "revision": "682e9fe3af4964739042",
+    "url": "/hbpddemo/static/css/main.578a2782.chunk.css"
+  },
+  {
+    "revision": "71a16e21f62b7d67ea5f",
+    "url": "/hbpddemo/static/js/2.90e94cbd.chunk.js"
+  },
+  {
+    "revision": "682e9fe3af4964739042",
+    "url": "/hbpddemo/static/js/main.e1956e2c.chunk.js"
+  },
+  {
+    "revision": "cde9ae99385bfd33f4d3",
+    "url": "/hbpddemo/static/js/runtime~main.52643c5f.js"
+  }
+]);
